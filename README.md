6342d78534253af7726620e17e237385c3f9e5f0
MetalTurtle <dekolis1218@gmail.com>
2021-10-24 12:15:57 -0700

    Add water tools

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Water Mod
 
-A mod for a modding competition
+### A mod for a modding competition
+
+This mod adds:
+* Water Pickaxe
+* Water Axe
+* Water Shovel
+* Water Hoe
+* Water Sword
+
+They are all crafted with the normal recipes using water buckets as the material. They have the strength of wooden tools. They all have 1 durability.
+
+These tool may seem useless at first glance—they do break very easily after all. But upon closer inspection, we see that these are representative of the human experience.
+The lack of durability in these tools is a metaphor for the pain and suffering we go through as humans. They are relatively difficult to craft, requiring lots of iron. This is analogous to the work we do as humans in society to get to where we want to be, sometimes without a reward at the end.
+By playing with this mod, you understand the immense thought that went into these items—their textures, their recipes, their uses—so that you remember what they represent as you play Minecraft.
--- src/main/java/io/github/metalturtle18/watermod/WaterMod.java
+++ src/main/java/io/github/metalturtle18/watermod/WaterMod.java
@@ -1,76 +1,119 @@
-package com.example.examplemod;
+package io.github.metalturtle18.watermod;
 
+import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.stream.Collectors;
-
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
-{
+@Mod("watermod")
+public class WaterMod {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final String MOD_ID = "watermod";
 
-    public ExampleMod() {
+    public WaterMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
+    private void setup(final FMLCommonSetupEvent event) {
         // some preinit code
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.messageSupplier().get()).
-                collect(Collectors.toList()));
-    }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
-        LOGGER.info("HELLO from server starting");
+        LOGGER.info("Welcome to Water Mod!");
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
+            final var registry = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+
+            // Register tools here because I don't feel like making a new class
+            registry.register(
+                    "water_pickaxe",
+                    () -> new PickaxeItem(
+                            Tiers.WOOD,
+                            1,
+                            -2.8F,
+                            new Item.Properties()
+                                    .tab(CreativeModeTab.TAB_TOOLS)
+                                    .durability(1)
+                    )
+            );
+
+            registry.register(
+                    "water_axe",
+                    () -> new AxeItem(
+                            Tiers.WOOD,
+                            1,
+                            -2.8F,
+                            new Item.Properties()
+                                    .tab(CreativeModeTab.TAB_TOOLS)
+                                    .durability(1)
+                    )
+            );
+
+            registry.register(
+                    "water_shovel",
+                    () -> new ShovelItem(
+                            Tiers.WOOD,
+                            1,
+                            -2.8F,
+                            new Item.Properties()
+                                    .tab(CreativeModeTab.TAB_TOOLS)
+                                    .durability(1)
+                    )
+            );
+
+            registry.register(
+                    "water_hoe",
+                    () -> new HoeItem(
+                            Tiers.WOOD,
+                            1,
+                            -2.8F,
+                            new Item.Properties()
+                                    .tab(CreativeModeTab.TAB_TOOLS)
+                                    .durability(1)
+                    )
+            );
+
+            registry.register(
+                    "water_sword",
+                    () -> new SwordItem(
+                            Tiers.WOOD,
+                            1,
+                            -2.8F,
+                            new Item.Properties()
+                                    .tab(CreativeModeTab.TAB_COMBAT)
+                                    .durability(1)
+                    )
+            );
+
+            registry.register(FMLJavaModLoadingContext.get().getModEventBus());
+            LOGGER.info("Water Mod registered tools");
         }
     }
 }
